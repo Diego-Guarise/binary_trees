2f0bde274963c236b2dd72bc076503e2f4982da1
Andres Ortiz <3283@holbertonschool.com>
2021-11-30 06:01:46 -0800

    Modified 9th structure

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,14 +12,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		if (tree->left)
-		{
 			height_left = binary_tree_height(tree->left) + 1;
-		}
-
 		if (tree->right)
-		{
 			height_right = binary_tree_height(tree->right) + 1;
-		}
 
 		if (height_left >= height_right)
 			return (height_left);
