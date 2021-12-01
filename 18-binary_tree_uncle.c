edec49fb2511062ec798740c24ceaa841c94f762
Andres Ortiz <3283@holbertonschool.com>
2021-11-30 20:57:07 -0800

    Added border condition for node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -25,7 +25,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node->parent)
+	if (!node || !node->parent)
 		return (NULL);
 
 	return (binary_tree_sibling(node->parent));
