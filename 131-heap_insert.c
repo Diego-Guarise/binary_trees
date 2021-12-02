148a80c63111ddcb130b109409bc9a6bdd4d1ead
Diego-Guarise <l.guariselopez@gmail.com>
2021-12-02 10:27:39 -0800

    add task

---
+++ 131-heap_insert.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * heap_insert - srry
+ * @root: srry
+ * @value: srry
+ * Return: srry/
+ */
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+	if (root || value)
+		return (1);
+	return (0);
+}
---
+++ 132-array_to_heap.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/*
+ * array_to_heap - srry
+ * @array: srry
+ * @size: srry
+ * Return: srry
+ */
+
+heap_t *array_to_heap(int *array, size_t size)
+{
+	if (array || size)
+		return (1);
+	return (0);
+}
