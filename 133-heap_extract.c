1b089682b39efe53e6f648915c56cdb9f414b81d
Diego-Guarise <l.guariselopez@gmail.com>
2021-12-02 10:16:58 -0800

    srry

---
+++ 133-heap_extract.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/*
+ * heap_extract - srry
+ * @root: srry
+ * Return: srry
+ */
+
+int heap_extract(heap_t **root)
+{
+	if (root)
+		return (1);
+	return (0);
+}
---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/*
+ * heap_to_sorted_array - srry
+ * @heap: srry
+ * @size: srry
+ * Return: srry
+ */
+
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	if (heap || size)
+		return (1);
+	return (0);
+}
