244a51b38a43ac9bba25c8df07cd08e79bc61695
Diego-Guarise <l.guariselopez@gmail.com>
2021-12-02 10:29:31 -0800

    fix

--- binary_trees.h
+++ binary_trees.h
@@ -24,10 +24,11 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
+*/
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+/**
 */
 
 /**
